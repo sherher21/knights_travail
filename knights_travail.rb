efa6b4e1cfd26a1afab5d31371512887e2bac957
Sher Her <sherher21@gmail.com>
2022-06-09 14:49:37 -0700

    updated knights move method

---
+++ knights_travail.rb
@@ -0,0 +1,45 @@
+class MoveTree
+  attr_reader :position, :parent
+  @@history = []
+  @@row_move = [2, 2, -2, -2, 1, 1, -1, -1]
+  @@col_move  = [-1, 1, 1, -1, 2,-2, 2, -2]
+
+  def initialize(position, parent)
+    @position = position
+    @parent = parent
+    @@history << position
+  end
+
+  def possible_moves(pos)
+    move_list = []
+    for i in 0..7
+      rank = @@row_move[i] + pos[0] 
+      file = @@col_move[i] + pos[1]
+      move_list << [rank, file] if rank.between?(0,7) && file.between?(0,7)
+    end
+    move_list
+  end
+
+  def children
+    move_list = possible_moves(@position)
+    move_list.reject! { |pos| @@history.include?(pos) }
+    move_list.map! { |posit| MoveTree.new(posit, self)}
+    return move_list
+  end
+end
+
+def shortest_path(node, list = [])
+  shortest_path(node.parent, list) unless node.parent.nil?
+  list << node.position
+end
+
+def knight_moves(start_position, end_position, queue = [], list = [])
+  node = MoveTree.new(start_position, nil)
+  until node.position == end_position
+    node.children.each { |child| queue << child }
+    node = queue.shift
+  end
+  shortest_path(node)
+end
+
+p knight_moves([0,0],[7,7])
